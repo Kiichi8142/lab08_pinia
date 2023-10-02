52b0af9edbca6aa3e0de0ba0b1f9b5cf30a3e3aa
Koharu2568 <vponsuwan@gmail.com>
2023-10-02 12:04:25 +0700

    change to setup store

--- src/store/cartStore.js
+++ src/store/cartStore.js
@@ -1,34 +1,59 @@
 import { defineStore } from "pinia";
+import { useProductStore } from "./productStore";
+import { ref, computed } from "vue";
 
-export const useCartStore = defineStore("cart", {
-	state: () => ({
-		cart: [],
-	}),
-	getters: {
-		getTotalCount() {
-			return this.cart.length;
-		},
-	},
-	actions: {
-		addItem(itemData) {
-			let cartItem = this.cart.find((item) => item.id === itemData.id);
-			if (cartItem) {
-				cartItem.count++;
-			} else {
-				this.cart.push(itemData);
-			}
-		},
-		removeItem(id) {
-			const itemIndex = this.cart.findIndex((item) => item.id === id);
-			if (itemIndex !== -1) {
-				this.cart.splice(itemIndex, 1);
-			}
-		},
-		setItemCount(id, count) {
-			let cartItem = this.cart.find((item) => item.id === id);
-			if (cartItem) {
-				cartItem.count = count;
-			}
-		},
-	},
+export const useCartStore = defineStore("cart", () => {
+	const cart = ref([]);
+
+	const itemCount = computed(() => cart.value.length);
+
+	const getItemPrice = computed(() => (id) => {
+		const productStore = useProductStore();
+		const item = cart.value.find((item) => item.id === id);
+		const price = item ? productStore.getProduct(item.id).price : 0;
+		return (parseFloat(price.replace("$", "")) * item.count).toFixed(2);
+	});
+
+	const totalPrice = computed(() => {
+		const productStore = useProductStore();
+		let sum = 0;
+		cart.value.forEach((item) => {
+			const product = productStore.getProduct(item.id);
+			sum += parseFloat(product.price.replace("$", "")) * item.count;
+		});
+		return sum.toFixed(2);
+	});
+
+	function addItem(itemData) {
+		const cartItem = cart.value.find((item) => item.id === itemData.id);
+		if (cartItem) {
+			cartItem.count++;
+		} else {
+			cart.value.push({ ...itemData, count: 1 });
+		}
+	}
+
+	function removeItem(id) {
+		const itemIndex = cart.value.findIndex((item) => item.id === id);
+		if (itemIndex !== -1) {
+			cart.value.splice(itemIndex, 1);
+		}
+	}
+
+	function setItemCount(id, count) {
+		const cartItem = cart.value.find((item) => item.id === id);
+		if (cartItem) {
+			cartItem.count = parseInt(count);
+		}
+	}
+
+	return {
+		cart,
+		itemCount,
+		getItemPrice,
+		totalPrice,
+		addItem,
+		removeItem,
+		setItemCount,
+	};
 });
